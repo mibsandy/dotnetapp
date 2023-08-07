7f016a36dfdac2c00603d4d05719f4d4678854ae
MFADLI28 <fadlidarmawan2804@gmail.com>
2023-08-07 11:20:59 +0700

    add new file

---
+++ msbuild_test_project/msbuild_test_project/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace msbuild_test_project
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World! from thetips4you");
+        }
+    }
+}
+
+
+
